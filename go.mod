7cc04f3a8896cac266877e96f8053f834be54824
caselongo <caselongo@gmail.com>
2021-01-09 12:54:08 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/leapforce-libraries/go_googleanalytics_reporting
+
+go 1.15
+
+require (
+	github.com/leapforce-libraries/go_errortools v0.0.0-20201204210737-d975f1003e2b
+	github.com/leapforce-libraries/go_google v0.0.0-20210109114719-3b4832a97f3d
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
+	google.golang.org/api v0.36.0
+)
---
+++ reporting.go
@@ -0,0 +1,62 @@
+package googleanalyticsreporting
+
+import (
+	"context"
+	"errors"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	google "github.com/leapforce-libraries/go_google"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/analyticsreporting/v4"
+	"google.golang.org/api/option"
+)
+
+const (
+	APIName string = "GoogleAnalyticsReporting"
+)
+
+// GoogleAnalyticsReporting stores GoogleAnalyticsReporting configuration
+//
+type ReportingService struct {
+	googleClient     *google.GoogleClient
+	ReportingService *analyticsreporting.Service
+}
+
+type TokenSource struct {
+	googleClient *google.GoogleClient
+}
+
+func (tokenSource TokenSource) Token() (*oauth2.Token, error) {
+	t, e := tokenSource.googleClient.ValidateToken()
+	if e != nil {
+		return nil, errors.New(e.Message())
+	}
+
+	return &oauth2.Token{
+		AccessToken:  *t.AccessToken,
+		TokenType:    *t.TokenType,
+		RefreshToken: *t.RefreshToken,
+		Expiry:       *t.Expiry,
+	}, nil
+}
+
+// methods
+//
+func NewReportingService(clientID string, clientSecret string, scope string, bigQuery *google.BigQuery) (*ReportingService, *errortools.Error) {
+	config := google.GoogleClientConfig{
+		APIName:      APIName,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scope:        scope,
+	}
+
+	googleClient := google.NewGoogleClient(config, bigQuery)
+
+	tokenSource := TokenSource{googleClient}
+
+	reportingService, err := analyticsreporting.NewService(context.Background(), option.WithTokenSource(tokenSource))
+	if err != nil {
+		return nil, errortools.ErrorMessage(err)
+	}
+	return &ReportingService{googleClient, reportingService}, nil
+}
